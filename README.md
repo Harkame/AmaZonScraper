0886de8bdc3603e008284c240b67e757a1bc9bd5
Harkame <louisr.daviaud@gmail.com>
2022-10-13 21:41:13 +0200

    dev

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # AmazonScraper
+
+## Installation
+
+``` bash
+
+pip install azc
+
+```
+
+OR
+
+clone this repository and
+
+``` bash
+
+pip install -r requirements.txt
+
+python setup.py install
+
+```
+
+## Usage
+
+### Initialization
+
+``` python
+
+from azc import AmazonScraper
+
+scraper = AmazonScraper()
+
+```
+
+### Get a product
+
+Use the product code
+
+``` python
+
+product = scraper.get_product("B08VH8C3WZ")
+print(str(product))
+
+"""
+
+Title : Western Digital WD Red Plus 3.5" 4000 Go Série ATA III
+Price : 119.13 €
+Evaluation : 4.6
+Evaluation count : 1
+Saving percentage : 10
+
+"""
+```
--- azs/azs.py
+++ azs/azs.py
@@ -16,12 +16,16 @@ DEFAULT_CREDENTIAL = "credential.json"
 
 
 class AmazonScraper:
-    def __init__(self):
-        self.session = requests.session()
+    def __init__(self, session=None, driver=None):
+        if session is None:
+            self.session = requests.session()
+        else:
+            self.session = session
 
-    def get_product(self, product_code):
-        url = f"{AMAZON_BASE_PRODUCT_URL}{product_code}"
-        print(url)
+        self.driver = driver
+
+    def __get_product_page__(self, product_url):
+        url = f"{product_url}"
 
         headers = {
             "User-Agent": UserAgent().random
@@ -29,17 +33,31 @@ class AmazonScraper:
 
         page = BeautifulSoup(self.session.get(url, headers=headers).content, "lxml")
 
-        product = Product()
-        product.url = url
+        return page
 
-        product_title_tag = page.find(id="titleSection")
+    def get_product(self, product_url):
+
+        page = self.__get_product_page__(product_url)
+
+        product = Product()
+        product.url = product_url
+
+        product_title_tag = product_url.find(id="titleSection")
 
         if product_title_tag is None:
             logger.debug("spam detected")
             time.sleep(15)  # spam detected
             return
 
-        image = page.find("img", {"id": "landingImage"})["src"]
+        tmp_evaluation = page.find('span', {'id': 'acrPopover'}).text
+        product.evaluation = float(re.findall(r'\d+,\d+', tmp_evaluation)[0].replace(',', '.'))
+
+        tmp_evaluationss_count = page.find('span', {'id': 'acrCustomerReviewText'}).text
+        product.evaluations_count = int(re.findall(r'\d+', tmp_evaluationss_count)[0])
+
+        tmp_saving_percentage = page.find('span', {'class' : 'savingsPercentage'}).text
+        product.saving_percentage = int(re.findall(r'\d+', tmp_saving_percentage)[0])
+
 
         price_block = page.find('span', {'class' : 'priceToPay'})
 
@@ -49,8 +67,6 @@ class AmazonScraper:
         tmp_price += price_block.find("span", {"class": "a-price-fraction"}).text
 
         tmp_price = tmp_price.replace(',', '.')
-        print(tmp_price)
-
         product.price = float(tmp_price)
 
         product.price_symbol = price_block.find("span", {"class": "a-price-symbol"}).text
@@ -65,6 +81,9 @@ class Product:
         self.url = None
         self.price = 0.0
         self.price_symbol = None
+        self.evaluation = 0.0
+        self.evaluations_count = 0
+        self.saving_percentage = 0.0
 
     def __str__(self):
         to_string = ""
@@ -72,12 +91,22 @@ class Product:
         to_string += self.title
         to_string += os.linesep
         to_string += "Price : "
-        to_string += str(self.price) + " " + self.price_symbolg
+        to_string += str(self.price) + " " + self.price_symbol
         to_string += os.linesep
+        to_string += "Evaluation : "
+        to_string += str(self.evaluation)
+        to_string += os.linesep
+        to_string += "Evaluation count : "
+        to_string += str(self.evaluations_count)
+        to_string += os.linesep
+        to_string += "Saving percentage : "
+        to_string += str(self.saving_percentage)
+        to_string += os.linesep
+
         return to_string
 
 
 if __name__ == "__main__":
     scraper = AmazonScraper()
-    product = scraper.get_product("B08VH8C3WZ")
+    product = scraper.get_product('https://www.amazon.fr/gp/product/B08VH8C3WZ')
     print(str(product))
